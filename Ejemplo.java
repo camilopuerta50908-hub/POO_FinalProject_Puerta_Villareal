6224d63a2d9f0eb191415b730d68392044847b70
camilopuerta50908-hub <camilo.puerta50908@ucaldas.edu.co>
2026-06-04 15:31:16 -0500

    Cambuios

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,9 +1,6 @@
 public class Ejemplo {
     public static void main(String[] args) {
         // Crear una instancia de la clase Persona
-        Persona persona1 = new Persona("Juan", 30);
-        
-        // Llamar al método mostrarInformacion
-        persona1.mostrarInformacion();
+        System.out.println("Creando una persona...");
     }
 }
