ba765432ce2b2cb4f8795244c57418e0a72182c1
camilopuerta50908-hub <camilo.puerta50908@ucaldas.edu.co>
2026-06-04 15:22:36 -0500

    Cambio

---
+++ Ejemplo.java
@@ -0,0 +1,9 @@
+public class Ejemplo {
+    public static void main(String[] args) {
+        // Crear una instancia de la clase Persona
+        Persona persona1 = new Persona("Juan", 30);
+        
+        // Llamar al método mostrarInformacion
+        persona1.mostrarInformacion();
+    }
+}
