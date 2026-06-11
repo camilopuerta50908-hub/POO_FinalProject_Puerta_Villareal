e635d497f933d6b7d1afe12a0530f50256e01ef2
camilopuerta50908-hub <camilo.puerta50908@ucaldas.edu.co>
2026-06-11 14:48:28 -0500

    xdd

--- Ejemplo.java
+++ Ejemplo.java
@@ -2,6 +2,8 @@ public class Ejemplo {
     public static void main(String[] args) {
         // Crear una instancia de la clase Persona
         System.out.println("Creando una personales...");
+        System.out.println("Creando una jermu...");
+        System.out.println("Creando un hijo...");
         
     }
 }
